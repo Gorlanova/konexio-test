70d11ff60b50419d380b94803ee96bc59ae3ab5a
Nina Gautreau <nina.gautreau@gmail.com>
2020-06-05 10:51:09 +0200

    create app and data

---
+++ src/App.js
@@ -0,0 +1,20 @@
+/// BASIC IMPORTS
+import React from 'react';
+
+/// PAGES & COMPONENTS
+
+
+/// STYLING
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      Hello Nina
+    </div>
+  );
+}
+
+export default App;
