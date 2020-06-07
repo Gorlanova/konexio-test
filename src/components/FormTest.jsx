a5fd87a810fda875eabceece194364ae2b614cdb
Nina Gautreau <nina.gautreau@gmail.com>
2020-06-07 16:50:16 +0200

    refactor handleSubmit with state

--- src/components/FormTest.jsx
+++ src/components/FormTest.jsx
@@ -20,7 +20,8 @@ export default class FormTest extends Component {
     password2: "",
     status: "",
     newsletter: false,
-    terms: false
+    terms: false,
+    submitted: false
   }
 
   handleChange = (e) => {
@@ -32,7 +33,9 @@ export default class FormTest extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    document.querySelector(".form-section").innerHTML = `<p class="green">Submitted !</p>`;
+    this.setState({
+      submitted: true
+    })
   }
 
   checkEmail = () => {
@@ -64,57 +67,65 @@ export default class FormTest extends Component {
     return (
       <div className="form-section">
         <p>Want to fill this gorgeous form ? Treat yourself :</p>
-        <form className="" action="" >
-
-          <TextField name="firstName" label="First Name" color="secondary" fullWidth={true} onChange={this.handleChange}/>
-
-          <TextField name="lastName" label="Last Name" color="secondary" fullWidth={true} onChange={this.handleChange} />
-
-          <FormControl>
-            <InputLabel id="status-label" color="secondary">Status</InputLabel>
-            <Select labelId="status-label" name="status" color="secondary" fullWidth={true} defaultValue="" onChange={this.handleChange}>
-              <MenuItem value={"Teacher"}>Teacher</MenuItem>
-              <MenuItem value={"TA"}>Teacher Assistant</MenuItem>
-              <MenuItem value={"Student"}>Student</MenuItem>
-            </Select>
-          </FormControl>
-          
-          <TextField id="emailInput" name="email" label="Email" required={true} color="secondary" error={this.checkEmail()} fullWidth={true} onChange={this.handleChange}/>
         
-          <TextField id="password1Input" name="password1" type="password" label="Password" required={true} color="secondary" error={this.checkPassword1()} fullWidth={true} onChange={this.handleChange}/>
 
-          <TextField id="password2Input" name="password2" type="password" label="Confirm Password" required={true} color="secondary" error={this.checkPassword2()} fullWidth={true} onChange={this.handleChange}/>
+        {!this.state.submitted ?
 
-          <FormControl>
-            <FormControlLabel
-              name="newsletter"
-              control={<Checkbox color="secondary" />}
-              label="I want to subscribe to the newsletter"
-              labelPlacement="end"
-              onChange={this.handleChange}
-            />
-            <FormHelperText></FormHelperText>
-          </FormControl>
+          <form className="" action="" >
+
+            <TextField name="firstName" label="First Name" color="secondary" fullWidth={true} onChange={this.handleChange}/>
+
+            <TextField name="lastName" label="Last Name" color="secondary" fullWidth={true} onChange={this.handleChange} />
+
+            <FormControl>
+              <InputLabel id="status-label" color="secondary">Status</InputLabel>
+              <Select labelId="status-label" name="status" color="secondary" fullWidth={true} defaultValue="" onChange={this.handleChange}>
+                <MenuItem value={"Teacher"}>Teacher</MenuItem>
+                <MenuItem value={"TA"}>Teacher Assistant</MenuItem>
+                <MenuItem value={"Student"}>Student</MenuItem>
+              </Select>
+            </FormControl>
+            
+            <TextField id="emailInput" name="email" label="Email" required={true} color="secondary" error={this.checkEmail()} fullWidth={true} onChange={this.handleChange}/>
           
+            <TextField id="password1Input" name="password1" type="password" label="Password" required={true} color="secondary" error={this.checkPassword1()} fullWidth={true} onChange={this.handleChange}/>
 
-          <FormControl required error={this.checkTerms()}>
-            <FormControlLabel
-              id="termsInput"
-              name="terms"
-              control={<Checkbox color="secondary" />}
-              label="I have read terms and conditions"
-              labelPlacement="end"
-              required={true}
-              onChange={this.handleChange}
-            />
-            <FormHelperText className="error-checkbox">You must agree to the terms and conditions</FormHelperText>
-          </FormControl>
+            <TextField id="password2Input" name="password2" type="password" label="Confirm Password" required={true} color="secondary" error={this.checkPassword2()} fullWidth={true} onChange={this.handleChange}/>
 
-          <Button className="button" variant="contained" disabled={this.checkError()} onClick={this.handleSubmit}>
+            <FormControl>
+              <FormControlLabel
+                name="newsletter"
+                control={<Checkbox color="secondary" />}
+                label="I want to subscribe to the newsletter"
+                labelPlacement="end"
+                onChange={this.handleChange}
+              />
+              <FormHelperText></FormHelperText>
+            </FormControl>
+            
+
+            <FormControl required error={this.checkTerms()}>
+              <FormControlLabel
+                id="termsInput"
+                name="terms"
+                control={<Checkbox color="secondary" />}
+                label="I have read terms and conditions"
+                labelPlacement="end"
+                required={true}
+                onChange={this.handleChange}
+              />
+              <FormHelperText className="error-checkbox">You must agree to the terms and conditions</FormHelperText>
+            </FormControl>
+            
+            <Button className="button" variant="contained" disabled={this.checkError()} onClick={this.handleSubmit}>
             Submit
-          </Button>
+            </Button>
 
-        </form>
+          </form>
+
+        : <h4 className="green-sent">Form sent !</h4>
+          
+        }
       </div>
     )
   }
